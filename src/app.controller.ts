21872563ed41dbfa2c9e0e5033d23ccfc600d4f1
Keihertz <kabesino@gmail.com>
2023-09-11 13:07:26 +0800

    updated landing page

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Request, Response } from 'express';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@Req() request: Request, @Res() response: Response): void {
+    const welcomeMessage = this.appService.getWelcomeMessage(request);
+    response.send(welcomeMessage);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Req } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getWelcomeMessage(@Req() request): string {
+    const currentUrl = `${request.protocol}://${request.get('host')}`;
+    const apiDocumentationUrl = `${currentUrl}/api`;
+
+    return `
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>Welcome to My Todo App</title>
+        </head>
+        <body>
+          <h1>Welcome to My Todo App</h1>
+          <p>
+            This is a simple CRUD application built with Nest.js and Firebase Realtime Database.
+          </p>
+          <p>
+            For more information, please refer to the API documentation:
+          </p>
+          <a href="${apiDocumentationUrl}">API Documentation</a>
+        </body>
+      </html>
+    `;
   }
 }
