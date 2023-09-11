c736bce3e07034044476b04716dd9527ea09d997
Keihertz <kabesino@gmail.com>
2023-09-11 12:36:02 +0800

    updated firebase to use .env

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,31 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import * as admin from 'firebase-admin';
+import * as dotenv from 'dotenv';
 
-// Firebase Admin SDK configuration
-const adminConfig = {
-  credential: admin.credential.cert('./firebase.json'),
-  databaseURL: 'https://to-do-app-be7a1-default-rtdb.firebaseio.com/',
-};
+// Load environment variables from .env file
+dotenv.config();
+
+// Initialize Firebase Admin SDK with service account credentials
+const adminConfig: admin.ServiceAccount = {
+  type: 'service_account',
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+} as any;
 
 // Initialize Firebase Admin SDK
-admin.initializeApp(adminConfig);
+admin.initializeApp({
+  credential: admin.credential.cert(adminConfig),
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+});
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
